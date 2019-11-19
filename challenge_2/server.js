3b275dd65e41479e403e9f94864d04f36e403ee8
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2019-11-19 12:04:31 -0800

    Set up html doc and basic server function

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const { $ } = require('jquery')
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Go Home World!!!');
+})
 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`)
+})
